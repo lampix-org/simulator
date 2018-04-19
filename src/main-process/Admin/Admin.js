b14f661caa1ac0a0726f582b4a198aa1f51c18e6
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-04-19 15:45:32 +0300

    Each simulator has its own browser
    This makes window management easier, since it removes the need for BrowserWindowManager, allowing each Simulator to create and control its own browser. It also makes some timing issues regarding passing the browser to the simulator and its preload.js more predictable

--- src/main-process/Admin/Admin.js
+++ src/main-process/Admin/Admin.js
@@ -1,5 +1,3 @@
-const path = require('path');
-const { browserWindowManager } = require('../BrowserWindowManager');
 const { Simulator } = require('../Simulator');
 const { initSimulatorSettingsListeners } = require('./ipc/initSimulatorSettingsListeners');
 
@@ -13,31 +11,19 @@ class Admin {
   loadApp(url) {
     console.log(`Admin.loadApp called with URL: ${url}`);
 
-    const { windows } = browserWindowManager;
-
-    let window = windows[url];
-
-    if (!window) {
-      console.log('Creating new window and simulator.');
-      window = browserWindowManager.newWindow({
-        id: url,
-        options: {
-          resizable: false,
-          webPreferences: {
-            preload: path.resolve(__dirname, '..', 'Simulator', 'preload.js'),
-            nodeIntegration: false
-          }
-        }
-      });
-
-      this.simulators[url] = new Simulator(window, url);
-      global[`simulator-${url}`] = this.simulators[url];
+    if (this.simulators[url]) {
+      return this.simulators[url];
     }
+
+    console.log('Creating new simulator...');
+
+    this.simulators[url] = new Simulator(url);
+    global[`simulator-${url}`] = this.simulators[url];
 
     const options = process.env.NODE_ENV === 'development' ? { extraHeaders: 'pragma: no-cache\n' } : {};
 
     console.log(`Loading app at ${url}`);
-    window.loadURL(url, options);
+    this.simulators[url].browser.loadURL(url, options);
 
     return this.simulators[url];
   }
--- src/main-process/Admin/createAdminBrowser.js
+++ src/main-process/Admin/createAdminBrowser.js
@@ -2,8 +2,8 @@ const { app } = require('electron');
 const path = require('path');
 
 module.exports = () => {
-  const { browserWindowManager } = require('../BrowserWindowManager');
-  const browser = browserWindowManager.newWindow({
+  const { newWindow } = require('../utils/newWindow');
+  const browser = newWindow({
     id: 'admin',
     onClosed: () => app.quit(),
     options: {
