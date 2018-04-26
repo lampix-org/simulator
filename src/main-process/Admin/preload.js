7e7727c64126be27c231eb7b78638899be0a0ed3
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-04-26 11:40:15 +0300

    Added renderer bindings for simulator settings in Admin/preload.js

--- src/main-process/Admin/preload.js
+++ src/main-process/Admin/preload.js
@@ -1,10 +1,26 @@
-const { remote } = require('electron');
+const { remote, ipcRenderer } = require('electron');
+const {
+  TOGGLE_MOVEMENT,
+  SET_CLASSIFIER,
+  SET_RECOGNIZED_CLASS,
+  SET_METADATA
+} = require('../ipcEvents');
 
 const { admin } = remote.require('./Admin');
 
 window.lampix = {
-  loadApp: (url) => {
-    const { settings } = admin.loadApp(url);
-    return settings;
-  }
+  loadApp: (url) => admin.loadApp(url),
+  toggleMovement: (url) => ipcRenderer.send(TOGGLE_MOVEMENT, { url }),
+  setClassifier: (url, classifier) => ipcRenderer.send(SET_CLASSIFIER, {
+    url,
+    classifier
+  }),
+  setRecognizedClass: (url, recognizedClass) => ipcRenderer.send(SET_RECOGNIZED_CLASS, {
+    url,
+    recognizedClass
+  }),
+  setMetadata: (url, metadata) => ipcRenderer.send(SET_METADATA, {
+    url,
+    metadata
+  })
 };
