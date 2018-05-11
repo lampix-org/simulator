ae7a475cb93065f7bfb46eb1b8ef355965b17928
Viorel Smeu <vsmeu@lampix.co>
2018-05-11 11:45:45 +0300

    Added lampix logo as launcher icon

--- src/main-process/utils/newWindow.js
+++ src/main-process/utils/newWindow.js
@@ -1,18 +1,27 @@
 const electron = require('electron');
 const noop = require('lodash.noop');
+const path = require('path');
 const mainDisplayCenterCoords = require('../utils/mainDisplayCenterCoords');
 const {
   DEFAULT_WINDOW_HEIGHT,
   DEFAULT_WINDOW_WIDTH
 } = require('../constants');
 
+
 const BrowserWindow = electron.BrowserWindow || electron.remote.BrowserWindow;
+
+const { nativeImage } = electron;
+const pathToLogo = path.join(__dirname, '../', '../', '/img', 'logo.png');
+const lampixLogo = nativeImage.createFromPath(pathToLogo);// __ '../../img/icon.png');
+console.log('lampixLogo ', lampixLogo);
+console.log('image.isEmpty() ', lampixLogo.isEmpty());
 
 const newWindow = ({
   width = DEFAULT_WINDOW_WIDTH,
   height = DEFAULT_WINDOW_HEIGHT,
   options = {},
-  onClosed = noop
+  onClosed = noop,
+  icon = lampixLogo
 } = {}) => {
   const { x, y } = mainDisplayCenterCoords(width, height);
 
@@ -21,9 +30,11 @@ const newWindow = ({
     height,
     x,
     y,
+    icon,
     useContentSize: true
   });
-
+  console.log('windowOptions ', windowOptions);
+  console.log('lampixLogo ', lampixLogo);
   const window = new BrowserWindow(windowOptions);
 
   // Emitted when the window is closed.
