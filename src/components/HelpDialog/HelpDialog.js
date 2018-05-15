1e6ca9404fe6e80a1338291fa986352f793caa99
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-05-15 20:12:28 +0300

    Added section about using the simulator

--- src/components/HelpDialog/HelpDialog.js
+++ src/components/HelpDialog/HelpDialog.js
@@ -184,6 +184,44 @@ const HelpDialog = ({
           &nbsp;available. Their contents will represent the registered areas that the simulator knows about.
         </Typography>
       </Paper>
+
+      <Paper className={classes.paper}>
+        <Typography variant="title">How to use the simulator?</Typography>
+        <Separator divider />
+
+        <Typography variant="subheading">Movement</Typography>
+        <Separator />
+
+        <Typography variant="body1">
+          Movement is fairly simple and straightforward. You only need to register areas with
+          &nbsp;<code>registerMovement</code> and activate the movement detector seen in
+          &nbsp;the settings image, then move your mouse through the rectangles you registered.
+
+          <Separator />
+
+          Note that this functionality is not yet available on the device. <br />
+        </Typography>
+        <Separator />
+
+        <Typography variant="subheading">Simple and position classification</Typography>
+        <Separator />
+
+        <Typography variant="body1">
+          Using <code>registerSimpleClassifier</code> or <code>registerPositionClassifier</code>
+          &nbsp;will make the simulator go through the <code>classifier</code>
+          &nbsp;property of each registered rectangle and provide them to you in the <strong>Classifier</strong>
+          &nbsp;select field in either the <strong>simple</strong> or <strong>position</strong>
+          &nbsp;categories.
+
+          <br />
+          <br />
+
+          The <strong>Recognized class</strong> select field in both categories
+          &nbsp;will show values from 0-10 and 100-110, by default.<br />
+          This is by design, as most classifiers only have classes between 0-10, and the values of some QR codes
+          &nbsp;we have used are between 100-110.
+        </Typography>
+      </Paper>
     </div>
   </Dialog>
 );
