a583e48c99075c7548a1b816d70cbe086a8aadce
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-05-24 15:33:00 +0300

    Updated ipcEvents values from lx/simulator to lx-simulator

--- src/main-process/ipcEvents.js
+++ src/main-process/ipcEvents.js
@@ -6,19 +6,19 @@ exports.UPDATE_SIMULATOR_SETTINGS = 'lx-simulator/IPC/UPDATE_SIMULATOR_SETTINGS'
 exports.UPDATE_SIMULATOR_LIST = 'lx-simulator/IPC/UPDATE_SIMULATOR_LIST';
 exports.UPDATE_URL_LIST = 'lx-simulator/IPC/UPDATE_URL_LIST';
 exports.INVALID_URL = 'lx-simulator/IPC/INVALID_URL';
-exports.ADMIN_UI_READY = 'lx/simulator/IPC/ADMIN_UI_READY';
-exports.CLOSE_SIMULATOR = 'lx/simulator/IPC/CLOSE_SIMULATOR';
-exports.FOCUS_SIMULATOR = 'lx/simulator/IPC/FOCUS_SIMULATOR';
-exports.LOAD_APP = 'lx/simulator/IPC/LOAD_APP';
-exports.GET_LAMPIX_INFO = 'lx/simulator/IPC/GET_LAMPIX_INFO';
+exports.ADMIN_UI_READY = 'lx-simulator/IPC/ADMIN_UI_READY';
+exports.CLOSE_SIMULATOR = 'lx-simulator/IPC/CLOSE_SIMULATOR';
+exports.FOCUS_SIMULATOR = 'lx-simulator/IPC/FOCUS_SIMULATOR';
+exports.LOAD_APP = 'lx-simulator/IPC/LOAD_APP';
+exports.GET_LAMPIX_INFO = 'lx-simulator/IPC/GET_LAMPIX_INFO';
 
 // Simulated app events
-exports.MOUSE_MOVE = 'lx/simulator/IPC/MOUSE_MOVE';
-exports.SIMPLE_CLICK = 'lx/simulator/IPC/SIMPLE_CLICK';
-exports.POSITION_CLICK = 'lx/simulator/IPC/POSITION_CLICK';
+exports.MOUSE_MOVE = 'lx-simulator/IPC/MOUSE_MOVE';
+exports.SIMPLE_CLICK = 'lx-simulator/IPC/SIMPLE_CLICK';
+exports.POSITION_CLICK = 'lx-simulator/IPC/POSITION_CLICK';
 
 // Simulator actions
-exports.REGISTER_MOVEMENT = 'lx/simulator/IPC/REGISTER_MOVEMENT';
-exports.REGISTER_SIMPLE = 'lx/simulator/IPC/REGISTER_SIMPLE';
-exports.REGISTER_POSITION = 'lx/simulator/IPC/REGISTER_POSITION';
-exports.TRANSFORM_COORDINATES = 'lx/simulator/IPC/TRANSFORM_COORDINATES';
+exports.REGISTER_MOVEMENT = 'lx-simulator/IPC/REGISTER_MOVEMENT';
+exports.REGISTER_SIMPLE = 'lx-simulator/IPC/REGISTER_SIMPLE';
+exports.REGISTER_POSITION = 'lx-simulator/IPC/REGISTER_POSITION';
+exports.TRANSFORM_COORDINATES = 'lx-simulator/IPC/TRANSFORM_COORDINATES';
