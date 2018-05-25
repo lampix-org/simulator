c922db7edd60ab7dca4bde77f4efc610be103d00
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-05-25 16:15:22 +0300

    Added SimulatorSettings help section

--- src/components/HelpDialog/HelpDialog.js
+++ src/components/HelpDialog/HelpDialog.js
@@ -18,14 +18,11 @@ import Paper from 'material-ui/Paper';
 import Separator from '../Separator';
 import InlineCode from '../InlineCode';
 
-// Assets
-import simulatorList from '../../assets/images/simulator-list.png';
-import simulatorSettings from '../../assets/images/simulator-settings.png';
-
 // Help sections
 import Tldr from './help-sections/Tldr';
 import WhatIsASimulator from './help-sections/WhatIsASimulator';
 import LoadingAnApp from './help-sections/LoadingAnApp';
+import SimulatorSettings from './help-sections/SimulatorSettings';
 
 const styles = (theme) => ({
   container: theme.mixins.gutters({
@@ -90,58 +87,10 @@ const HelpDialog = ({
         imageClass={classes.image}
       />
 
-      <Paper className={classes.paper}>
-        <Typography variant="title">Simulator settings</Typography>
-        <Separator divider />
-
-        <Typography variant="body1">
-          If we had three simulators open at the same time, the list would look like this:
-        </Typography>
-
-        <Separator />
-        <img
-          className={classes.image}
-          src={simulatorList}
-          alt="Simulator list"
-        />
-        <Separator />
-
-        <Typography variant="body1">
-          Expanding a simulator opens up the available settings, as seen in the following image:
-        </Typography>
-
-        <Separator />
-        <img
-          className={classes.image}
-          src={simulatorSettings}
-          alt="Simulator settings"
-        />
-        <Separator />
-
-        <Typography variant="body1">
-          <strong>
-          Note how <i>classifier</i>, <i>recognized class</i> and <i>metadata</i>
-          &nbsp;are available in two categories: <i>simple</i> and <i>position</i>.
-          </strong>
-          <br />
-
-          These represent the types of classification Lampix can do. <br /><br />
-
-          Simple classification will inform you whether a recognized object has appeared in a specified area,
-          &nbsp;and what class the object belongs to, and it expects to have only one object in that area.<br />
-
-          Position classification, on the other hand, works in two steps.
-
-          First, it provides the outline of the detected object(s) as a preliminary
-          &nbsp;step before classification is complete.
-
-          Then, when classification is done (per object), the device returns an array
-          &nbsp;of JSON objects describing what the device detected and recognized.
-          These JSON objects provide information about the outline (and therefore position)
-          &nbsp;and the recognized class of the detected objects.
-        </Typography>
-        <Separator />
-      </Paper>
+      <SimulatorSettings
+        className={classes.paper}
+        imageClass={classes.image}
+      />
 
       <Paper className={classes.paper}>
         <Typography variant="title">Registered areas</Typography>
