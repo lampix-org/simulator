cfdb2b318b84d7621aaaeb596fa79bf1c2a38910
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-05-30 11:38:30 +0300

    Show notification when dropping file is possible

--- src/containers/Core/Core.js
+++ src/containers/Core/Core.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
-import { withStyles } from 'material-ui/styles';
 
 import { ADMIN_UI_READY } from '../../main-process/ipcEvents';
 
 import ButtonAppBar from '../../components/ButtonAppBar';
 import SimulatorList from '../SimulatorList';
-
-const styles = (theme) => ({
-  simulatorListContainer: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
-    minHeight: 'calc(100% - 85px)',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  })
-});
+import SimulatorListContainer from '../SimulatorList/SimulatorListContainer';
 
 class Core extends Component {
   componentDidMount() {
@@ -24,21 +12,16 @@ class Core extends Component {
   }
 
   render() {
-    const { classes } = this.props;
     return (
       <React.Fragment>
         <ButtonAppBar />
 
-        <div className={classes.simulatorListContainer}>
+        <SimulatorListContainer>
           <SimulatorList />
-        </div>
+        </SimulatorListContainer>
       </React.Fragment>
     );
   }
 }
 
-Core.propTypes = {
-  classes: PropTypes.object.isRequired // eslint-disable-line
-};
-
-export default withStyles(styles)(Core);
+export default Core;
---
+++ src/containers/SimulatorList/SimulatorListContainer.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Material UI
+import { withStyles } from 'material-ui/styles';
+import Snackbar from 'material-ui/Snackbar';
+
+const styles = (theme) => ({
+  container: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    minHeight: 'calc(100% - 85px)',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative'
+  })
+});
+
+const preventDefault = (e) => e.preventDefault();
+
+class SimulatorListContainer extends React.Component {
+  state = { dragging: false };
+
+  componentDidMount() {
+    document.ondragover = preventDefault;
+    document.ondrop = preventDefault;
+
+    this.container.addEventListener('dragenter', () => {
+      this.setState({
+        dragging: true
+      });
+    });
+
+    this.container.addEventListener('dragleave', () => {
+      this.setState({
+        dragging: false
+      });
+    });
+  }
+
+  render() {
+    const { classes, children } = this.props;
+    const { dragging } = this.state;
+
+    return (
+      <div
+        className={`${classes.container} ${dragging ? 'dragging' : ''}`}
+        ref={(c) => { this.container = c; }}
+      >
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={dragging}
+          message={<span>Drop the file to load it</span>}
+        />
+
+        {children}
+      </div>
+    );
+  }
+}
+
+SimulatorListContainer.propTypes = {
+  classes: PropTypes.shape({
+    container: PropTypes.string
+  }).isRequired,
+  children: PropTypes.node.isRequired
+};
+
+export default withStyles(styles)(SimulatorListContainer);
