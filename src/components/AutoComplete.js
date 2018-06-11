854f39e81d5b2899024f485fa4114e5de7d0a29f
Dan Balaban <dan.stefan.balaban@gmail.com>
2018-06-11 10:24:03 +0300

    Changed MenuItem import to match Material UI v1.2.0

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Downshift from 'downshift';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import { MenuItem } from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
