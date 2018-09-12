33e07e727e8384aad3b7dc03be7ab5092dcd597d
Dan Balaban <dbalaban@lampix.co>
2018-09-12 10:29:20 +0300

    Shapes enum in constants

--- src/main-process/constants.js
+++ src/main-process/constants.js
@@ -6,3 +6,9 @@ exports.DEFAULT_CLASSES = [
   ...(range(0, 10)).map((n) => n.toString()),
   ...(range(100, 110)).map((n) => n.toString())
 ];
+
+exports.Shapes = Object.freeze({
+  Rectangle: 'rectangle',
+  Circle: 'circle',
+  Polygon: 'polygon'
+});
