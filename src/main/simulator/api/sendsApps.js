ee8828263f95fc8373c6551507ccd121b6276270
Dan Balaban <dbalaban@lampix.co>
2018-12-17 11:51:09 +0200

    [sendsApps] Simulator package.json and is_local

--- src/main/simulator/api/sendsApps.js
+++ src/main/simulator/api/sendsApps.js
@@ -1,6 +1,21 @@
 const { response } = require('./response');
 const { respond } = require('./respond');
 
+const dummyPackageJson = (i) => ({
+  name: `app-${i}`,
+  version: `${i}.1.0`,
+  displayName: `App #${i}`,
+  author: {
+    name: `X Dev ${i}`,
+    email: `x${i}@dev.com`
+  },
+  description: 'Dummy app provided by the simulator',
+  lampixConfig: {
+    lampixVersion: '>= 2.1.0',
+    showInAppSwitcher: true
+  }
+});
+
 const sendsApps = (
   state,
   browser,
@@ -9,7 +24,11 @@ const sendsApps = (
   sendApps(requestJson) {
     const req = JSON.parse(requestJson);
     const numberOfDummyApps = configStore.get('simulator.appSwitcher.numberOfDummyApps');
-    const dummyApps = Array.from({ length: numberOfDummyApps }, (v, i) => ({ name: `App #${i}` }));
+    const dummyApps = Array.from({ length: numberOfDummyApps }, (v, i) => ({
+      name: `app-#${i}`,
+      package_data: dummyPackageJson(i),
+      is_local: i % 2 === 0
+    }));
 
     const res = response(req.request_id, null, {
       apps: dummyApps
